9ca67f26ac6302825b217c95ecf6767e86679a46
AndriianaMazan <64812682+AndriianaMazan@users.noreply.github.com>
2020-11-26 11:10:52 +0200

    changed bugs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,24 +9,24 @@ public class Main {
         intList.add(283097109);
         System.out.println("Array #1.1");
         intList.print();
-        if(intList.getLargest() != null) {
+        if (intList.getLargest() != null) {
             int maxInt = intList.getLargest();
             int minInt = intList.getSmallest();
             System.out.println("The largest number: " + maxInt);
-            System.out.println("The smalles}t number: " + minInt);
+            System.out.println("The smallest number: " + minInt);
         }
         System.out.println();
 
-        MyList<Double> myList = new MyList();
-        myList.add(9.3453);
-        myList.add(0.983097109);
+        MyList<Double> doubleList = new MyList();
+        doubleList.add(9.3453);
+        doubleList.add(0.983097109);
         System.out.println("Array #1.2");
-        myList.print();
-        if(myList.getLargest() != null) {
-            double maxDouble = myList.getLargest();
-            double minDouble = myList.getSmallest();
+        doubleList.print();
+        if (doubleList.getLargest() != null) {
+            double maxDouble = doubleList.getLargest();
+            double minDouble = doubleList.getSmallest();
             System.out.println("The largest number: " + maxDouble);
-            System.out.println("The getSmallest number: " + minDouble);
+            System.out.println("The smallest number: " + minDouble);
         }
         System.out.println();
 
@@ -36,11 +36,11 @@ public class Main {
         intListComparable.add(983097109);
         System.out.println("Array #2.1");
         intListComparable.print();
-        if(intListComparable.getLargest() != null) {
+        if (intListComparable.getLargest() != null) {
             int maxIntComparable = intListComparable.getLargest();
             int minIntComparable = intListComparable.getSmallest();
             System.out.println("The largest number of Comparable Integer List: " + maxIntComparable);
-            System.out.println("The getSmallest number of Comparable Integer List: " + minIntComparable);
+            System.out.println("The smallest number of Comparable Integer List: " + minIntComparable);
         }
         System.out.println();
 
@@ -49,11 +49,11 @@ public class Main {
         stringListComparable.add("There is more amount of words here");
         System.out.println("Array #2.2");
         stringListComparable.print();
-        if(stringListComparable.getLargest() != null) {
+        if (stringListComparable.getLargest() != null) {
             String maxStringComparable = stringListComparable.getLargest();
             String minStringComparable = stringListComparable.getSmallest();
             System.out.println("The largest number of Comparable String List: " + maxStringComparable);
-            System.out.println("The getSmallest number of Comparable String List: " + minStringComparable);
+            System.out.println("The smallest number of Comparable String List: " + minStringComparable);
         }
         System.out.println();
 
@@ -62,22 +62,22 @@ public class Main {
         ElementsArray stringArray = new ElementsArray(strings);
         System.out.println("Array #3.1");
         stringArray.print();
-        if(stringArray.getHighest() != null) {
-            String maxStringArray = stringListComparable.getLargest();
-            String minStringArray = stringListComparable.getSmallest();
+        if (stringArray.getHighest().toString() != null) {
+            String maxStringArray = stringArray.getHighest().toString();
+            String minStringArray = stringArray.getLowest().toString();
             System.out.println("\nThe largest number of Comparable String Array: " + maxStringArray);
-            System.out.println("The getSmallest number of Comparable String Array: " + minStringArray);
+            System.out.println("The smallest number of Comparable String Array: " + minStringArray);
         }
 
         Integer[] integers = {1, 2, 3, 4, 5};
         ElementsArray intArray = new ElementsArray(integers);
         System.out.println("Array #3.2");
         intArray.print();
-        if(intArray.getHighest() != null) {
-            String maxIntegerArray = stringListComparable.getLargest();
-            String minIntegerArray = stringListComparable.getSmallest();
+        if (intArray.getHighest() != null) {
+            int maxIntegerArray = Integer.parseInt(intArray.getHighest().toString());
+            int minIntegerArray = Integer.parseInt(intArray.getLowest().toString());
             System.out.println("\nThe largest number of Comparable Integer Array: " + maxIntegerArray);
-            System.out.println("The getSmallest number of Comparable Integer Array: " + minIntegerArray);
+            System.out.println("The smallest number of Comparable Integer Array: " + minIntegerArray);
         }
     }
 }
